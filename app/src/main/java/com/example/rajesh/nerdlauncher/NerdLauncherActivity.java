4e8a8685248cfa6e2b8525a1a16d64ab3d0b8f77
squashmeister99 <squashmeister99@gmail.com>
2017-02-08 11:37:22 -0700

    nerd launcher

---
+++ app/src/main/java/com/example/rajesh/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,11 @@
+package com.example.rajesh.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
